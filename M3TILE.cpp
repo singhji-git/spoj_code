f7afb066c3a8d5319fd018939b334a1d27d18ca1
singhji-git <sauarvk048@gmail.com>
2017-01-19 20:39:44 +0530

    just increasing the size of array and improving the indentation

--- M3TILE.cpp
+++ M3TILE.cpp
@@ -5,8 +5,7 @@ int main()
 	int n;
 	cin>>n;
 	while(n!=-1){
-	
-	int arr[40];
+	int arr[41];
 	for(int i=0;i<=30;i++)
 	{
 		arr[i]=0;
@@ -15,10 +14,9 @@ int main()
 	arr[1]=0;
 	arr[2]=3;
 	arr[3]=0;
-	
 	for(int i=4;i<=30;i+=2)
 	{
-		arr[i]=arr[i-2]*4-arr[i-4];
+	    arr[i]=arr[i-2]*4-arr[i-4];
 	}
 	cout<<arr[n]<<endl;
 	cin>>n;
